0edbbb1ca1288119fdf7e2480c55a00b576da714
Daniel Braga <daniel@MacBook-Pro-de-Daniel.local>
2025-04-30 09:17:30 -0300

    projsala

--- projsala/projsala/urls.py
+++ projsala/projsala/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("imc/",include('imc.urls')),
-    path("imc/calcular",include('imc.urls'))
+    path("imc/calcular/",include('imc.urls'))
 ]
