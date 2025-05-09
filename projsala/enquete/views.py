b70e8f080d45b3322873b35bfd75c6ed98f6e82f
Daniel Braga <danielfranciscobragadearaujo@gmail.com>
2025-05-09 10:26:05 -0300

    enquete v6

--- projsala/enquete/views.py
+++ projsala/enquete/views.py
@@ -4,8 +4,16 @@ opc = 0
 lista = [0,0,0,0]
 numero_votos = 0
 
+alternativas = ["Opção 1: ", "Opção 2: ", "Opção 3: ", "Opção 4: "]
+pergunta = "Escolha uma opção:"
+
 def index(request):
-    return render(request, 'enquete.html')
+
+    contexto = {
+        'pergunta': pergunta,
+        'alternativas': alternativas
+    }
+    return render(request, 'enquete.html', contexto)
 
 def resultado(request):    
     global numero_votos
@@ -16,12 +24,13 @@ def resultado(request):
 
     numero_votos += 1
 
+
+    ziped_list = zip(alternativas, lista)
+
     contexto = {
-        'opc': lista[0],
-        'opc2': lista[1],
-        'opc3': lista[2],
-        'opc4': lista[3],
-        'numero_votos': numero_votos
+        'resultado': ziped_list,
+        'numero_votos': numero_votos,
+        'pergunta': pergunta
     }
 
     return render(request, 'resultado.html', contexto)
